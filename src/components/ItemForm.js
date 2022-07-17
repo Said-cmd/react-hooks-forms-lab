4d1a6fe51823c25a0de7b6a19adf891ec216b6aa
Said Hussein <saidhussein12342@gmail.com>
2022-07-17 14:44:16 +0300

    commit tree ff219dc583fc4b5383a51e13617f490a262c8d69

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
+ function AbstractedItemForm({ onItemFormSubmit }) {
+   const [formData, setFormData] = useState({
+     name: "",
+     category: "Produce",
+   });
 
-      <label>
-        Category:
-        <select name="category">
+   function handleChange(event) {
+     setFormData({
+       ...formData,
+       [event.target.name]: event.target.value,
+     });
+   }
+
+   function handleSubmit(event) {
+     event.preventDefault();
+     onItemFormSubmit({
+       id: uuid(),
+       ...formData
+     });
+   }
+
+   return (
+     <form className="NewItem" onSubmit={handleSubmit}>
+       <label>
+         Name:
+         <input
+           type="text"
+          name="name"
+           value={formData.name}
+           onChange={handleChange}
+        />
+       </label>
+
+       <label>
+         Category:
+     <select
+          name="category"
+         value={formData.category}
+      onChange={handleChange}
+      >
           <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
+        <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
-      </label>
+       </label>
 
-      <button type="submit">Add to List</button>
-    </form>
-  );
-}
+       <button type="submit">Add to List</button>
+     </form>
+   );
+ }
 
-export default ItemForm;
+export default AbstractedItemForm;
